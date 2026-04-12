9e1f052d77d36a99da9293c40518b065cb5b46bb
nikita5jaiswal-hash <nikita5jaiswal@gmail.com>
2026-04-12 18:31:13 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> maps = new HashMap<>();
+        HashMap<Character,Integer> mapt = new HashMap<>();
+
+        for(char c: s.toCharArray()) {
+            maps.put(c,maps.getOrDefault(c,0)+1);
+        }
+         for(char c: t.toCharArray()) {
+            mapt.put(c,mapt.getOrDefault(c,0)+1);
+        }
+
+        boolean isAna = true;
+        if(maps.size()!= mapt.size()) {
+            isAna = false;
+        }
+        for(Map.Entry<Character,Integer> entry: maps.entrySet()) {
+            if(maps.size()!= mapt.size()) {
+            isAna = false;
+            break;
+        }
+            char c = entry.getKey();
+            int val = entry.getValue();
+            if(!mapt.containsKey(c) || mapt.get(c)!= val) {
+                isAna = false;
+                break;
+            }
+        }
+        return isAna;
+    }
+}
